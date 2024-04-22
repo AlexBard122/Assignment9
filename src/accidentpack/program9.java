a2624449075d9d3375d9a312c0d9b903148c4b6e
AlexBard122 <alexander.bard@maine.edu>
2024-04-22 12:24:25 -0500

    Added README & code cleanup

--- src/accidentpack/program9.java
+++ src/accidentpack/program9.java
@@ -2,7 +2,6 @@
  * 
  */
 package accidentpack;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
@@ -19,16 +18,9 @@ public class program9 {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		
 
 		//argument for input file
 		String filePath = args[0]; 		// EX: accidents_small_sample.csv
-		
-		//arguments for user input
-//		String state = args[1]; 		// EX: IL
-//		String stringDate = args[2];	// EX: 2022-09-08
-//		LocalDateTime date = ReportHelper.dateTimeConvert(args[2].concat(" 00:00:00"));
 		
 		//creates the treemap of arrays and prints the time to do so
 		TreeMap<String, ArrayList<report>> report = makeTreeMap(filePath);
@@ -37,7 +29,7 @@ public class program9 {
 		//test the sorting method for arrays
 		//testSort(state, report);
 		
-		//create a copy of the treemap and sort it 10 times, reporting the avg time to sort
+		//create a copy of the treemap and sort it, reporting the time to sort
 		sortTimer(report);
 		
 	}
@@ -47,35 +39,31 @@ public class program9 {
 	 * determines how long it takes to sort an arraylist
 	 */
 	private static void sortTimer(TreeMap<String, ArrayList<report>> report) {
-//		long startTime;
-//		long endTime;
-//		double processTime;
-//		double avgTime = 0.0;
-		
 			
-//		partitionWithFirst(report);
+		//IMPORTANT: partitionWithFirst & partitionWithLast methods will overflow when executed after any
+		//			 other method. partitionWithMedianOf10 will may not work on machines with poor RAM.
+		//			 partitionWithMidOld is a version of partitionWithMid that doesn't use insertion sort.
+		
+		partitionWithFirst(report);
 			
 //		partitionWithLast(report);
 		
-//		partitionWithMedianOf10(report);
+		partitionWithMedianOf10(report);
 		
-//		partitionWithMedianOf3(report);
+		partitionWithMedianOf3(report);
 		
 		partitionWithMid(report);
 		
-//		partitionWithRandom(report);
-			
-			//endTime = System.nanoTime();
-			//processTime = ReportHelper.convertTime(startTime, endTime);
-			//System.out.println(processTime + " Miliseconds to sort the treemap #" + i);
-			//avgTime = avgTime + processTime;
+		partitionWithMidOld(report);
 		
-		//avgTime = avgTime / 10.0;
-		//System.out.println(avgTime + " Miliseconds on average to sort the treemap");
+		partitionWithRandom(report);
+			
 	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * prints how long it takes to sort an array with random partitioning
 	 */
 	private static void partitionWithRandom(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
@@ -95,7 +83,9 @@ public class program9 {
 	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * prints how long it takes to sort an array with mid partitioning
 	 */
 	private static void partitionWithMid(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
@@ -113,9 +103,33 @@ public class program9 {
 		}
 		System.out.println("----------------------------");
 	}
+	
+	/**
+	 * @author abard
+	 * @param report
+	 * prints how long it takes to sort an array with mid partitioning (Old Version)
+	 */
+	private static void partitionWithMidOld(TreeMap<String, ArrayList<report>> report) {
+		long startTime;
+		long endTime;
+		TreeMap<String, ArrayList<report>> reportCopy = new TreeMap<String, ArrayList<report>>();
+		reportCopy.putAll(report);
+		System.out.println("Partitioning with Mid:");
+		for(Map.Entry<String, ArrayList<report>> reports : reportCopy.entrySet()) {
+			startTime = System.nanoTime();
+			//CHANGE THIS LINE TO CALL DIFFERENT SORTING ALGORITHMS
+			QuickSortMidNoInsertionSort.quickSort(reports.getValue());
+			endTime = System.nanoTime();
+			System.out.println(reports.getKey() + ": " + reports.getValue().size() + " Reports  " +
+					"\t" + ReportHelper.convertTime(startTime, endTime) + " Miliseconds");
+		}
+		System.out.println("----------------------------");
+	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * prints how long it takes to sort an array with median if 3 partitioning
 	 */
 	private static void partitionWithMedianOf3(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
@@ -135,7 +149,9 @@ public class program9 {
 	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * prints how long it takes to sort an array with median of 10 partitioning
 	 */
 	private static void partitionWithMedianOf10(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
@@ -155,7 +171,9 @@ public class program9 {
 	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * prints how long it takes to sort an array with last partitioning
 	 */
 	private static void partitionWithLast(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
@@ -175,7 +193,9 @@ public class program9 {
 	}
 
 	/**
+	 * @author abard
 	 * @param report
+	 * Prints how long it takes to sort an array with first partitioning
 	 */
 	private static void partitionWithFirst(TreeMap<String, ArrayList<report>> report) {
 		long startTime;
